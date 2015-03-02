2ff0581bc97ac9bf955afa7f5c9d8f71b9f60589
vagrant <vagrant@precise64.(none)>
2015-03-02 21:04:08 +0000

    updated recipe based on latest advice from Tejay.

--- cookbooks/database/recipes/default.rb
+++ cookbooks/database/recipes/default.rb
@@ -8,6 +8,14 @@ end
 mysql_config 'default' do
   source 'my.cnf.erb'
   cookbook 'mysql'
+  variables :config => {
+              :name => "mysql",
+              :port => 3306,
+              :user => "mysql"
+            },
+            :pid_file => "/var/run/mysqld/mysqld.pid",
+            :socket_file => "/var/run/mysqld/mysqld.sock",
+            :include_dir => "/etc/mysql/conf.d/"
   notifies :restart, 'mysql_service[default]'
   action :create
 end
